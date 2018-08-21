f853534ca0cc13580f878d0571234399c9b6ab31
Jude Beadle <info@jbeadle.de>
2018-08-21 23:47:11 +0200

    feat: initiate map with url argument and paramenters

--- src/App.js
+++ src/App.js
@@ -3,19 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+// Lifecycle Event - first call to do stuff
+
+componentDidMount(){
+  this.loadMap("http://maps.googleapis.com/maps/api/js?key=AIzaSyDT_aopfo8YkyGYkbDGcuwtVEHCl3k4Ays&v=3&callback=initMap")
+}
+
+// Load Map
+
+loadMap = (url) => {
+  const index = window.document.getElementsByTagName("script")[0]
+  const script = window.document.createElement("script")
+  script.src = url 
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
+  window.initMap = this.initMap
+}
+
+// initialize map
+
+initMap = () => {
+
+  const map = new window.google.maps.Map(document.getElementById('map'), {
+    // set center point
+    center: {lat: 52.52000659999999, lng: 13.404954},
+    // set zoom level
+    zoom: 13,
+    // set styles - see https://mapstyle.withgoogle.com/
+    styles: []
+  });
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* Create a place to put the map in the HTML */}
+        <div id="map">map</div>
+
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ src/Map.js
@@ -0,0 +1,1 @@
+// TODO move map component here
