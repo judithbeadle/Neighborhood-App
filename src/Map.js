4f159cb6846b56bea0c9b6e9991071710a5be33e
Jude Beadle <info@jbeadle.de>
2018-08-22 21:35:42 +0200

    feat: creating map component

--- src/Map.js
+++ src/Map.js
@@ -1,1 +1,12 @@
 // TODO move map component here
+import React, { Component } from 'react';
+
+class Map extends Component {
+  render(){
+    return(
+      <div>Map Template</div>
+    );
+  }
+};
+
+export default Map;
