270d4b3f2c6f0ca36a2f47be09334a31a5224c9f
Jude Beadle <info@jbeadle.de>
2018-08-22 19:14:33 +0200

    feat: Sidebar Component as Container for List

--- src/App.js
+++ src/App.js
@@ -1,54 +1,56 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Sidebar.js';
 
 class App extends Component {
 
-// Lifecycle Event - first call to do stuff
+  // Lifecycle Event - first call to do stuff
 
-componentDidMount(){
-  this.loadMap("http://maps.googleapis.com/maps/api/js?key=AIzaSyDT_aopfo8YkyGYkbDGcuwtVEHCl3k4Ays&v=3&callback=initMap")
-}
+  componentDidMount(){
+    this.loadMap("http://maps.googleapis.com/maps/api/js?key=AIzaSyDT_aopfo8YkyGYkbDGcuwtVEHCl3k4Ays&v=3&callback=initMap")
+  }
 
-// Load Map
+  // Load Map
 
-loadMap = (url) => {
-  const index = window.document.getElementsByTagName("script")[0]
-  const script = window.document.createElement("script")
-  script.src = url 
-  script.async = true
-  script.defer = true
-  index.parentNode.insertBefore(script, index)
-  window.initMap = this.initMap
-}
+  loadMap = (url) => {
+    const index = window.document.getElementsByTagName("script")[0]
+    const script = window.document.createElement("script")
+    script.src = url 
+    script.async = true
+    script.defer = true
+    index.parentNode.insertBefore(script, index)
+    window.initMap = this.initMap
+  }
 
-// initialize map
+  // initialize map
 
-initMap = () => {
+  initMap = () => {
 
-  const map = new window.google.maps.Map(document.getElementById('map'), {
-    // set center point
-    center: {lat: 52.52000659999999, lng: 13.404954},
-    // set zoom level
-    zoom: 13,
-    // set styles - see https://mapstyle.withgoogle.com/
-    styles: []
-  });
-}
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      // set center point
+      center: {lat: 52.52000659999999, lng: 13.404954},
+      // set zoom level
+      zoom: 13,
+      // set styles - see https://mapstyle.withgoogle.com/
+      styles: []
+    });
+  }
 
   render() {
-    return (
-      <div className="App">
-        {/* Create a place to put the map in the HTML */}
-        <div id="map">map</div>
-        <div id="sidebar">
-        Here goes the sidebar
+      return (
+        <div className="App">
+          {/* Create a place to put the map in the HTML */}
+          <div id="map">map</div>
+          <div id="sidebar">
+
+            <Sidebar />
+
+          </div>
 
         </div>
-
-      </div>
-    );
-  }
+      );
+    }
 }
 
 
---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Sidebar extends Component{
+	// the sidebar holds the list of places
+	render(){
+		return(
+			
+			<h2>In the Neighborhood</h2>
+			
+		)
+	}
+}
+
+export default Sidebar
