394966e6093e7e695ab80c676f1c394787b6ec46
Jude Beadle <info@jbeadle.de>
2018-08-22 19:22:12 +0200

    feat: add compontent to hold individual List Items

---
+++ src/ListItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class ListItem extends Component{
+	// the sidebar holds the list of places
+	render(){
+		return(
+			
+			<li>Location Name</li>
+			
+		)
+	}
+}
+
+export default ListItem
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
+import ListItem from './ListItem'
 
 class Sidebar extends Component{
 	// the sidebar holds the list of places
 	render(){
 		return(
-			
-			<h2>In the Neighborhood</h2>
-			
+			<div className="List">
+				<h2>In the Neighborhood</h2>
+				<ol>
+					<ListItem />
+				</ol>
+			</div>
 		)
 	}
 }
