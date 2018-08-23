99ee2e2ce83ee91b8b3c1d5b3a339dad184bf86f
Jude Beadle <info@jbeadle.de>
2018-08-23 21:45:35 +0200

    adding an alternative Map component - keeping the previous approach in oldMap Component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Map from './Map.js';
 class App extends Component {
 
   locations = [
-    { name: 'Location Name' },
+    { name: 'Location Nme' },
     { name: 'reggrvdv' },
     { name: 'tbsbt' },
     { name: 'bvtstvrt' },
@@ -23,7 +23,13 @@ class App extends Component {
           {/* Create a place to put the map in the HTML */}
           <div id="map">
 
-            <Map />
+            <Map
+              googleMapURL="http://maps.googleapis.com/maps/api/js?key=AIzaSyBDrid_nZpJckzqU2t51-aGhgxd-LjeDD4&v=3.exp&libraries=geometry,drawing,places"
+              loadingElement={<div className="loading-element" />}
+              containerElement={<div style={{ height: `400px` }} />}
+              mapElement={<div style={{ height: `100%` }} />}
+            />
+
           </div>
           <div id="sidebar">
 
