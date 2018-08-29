efc2493774e2748428d1639bf240327b4e037170
Jude Beadle <info@jbeadle.de>
2018-08-29 20:58:21 +0200

    updating color for the polygon on google map

--- src/Map.js
+++ src/Map.js
@@ -31,10 +31,10 @@ class Map extends Component {
     // Construct the polygon.
     const area = new window.google.maps.Polygon({
       paths: coordinates,
-      strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: '#FF0000',
+      strokeColor: '#edc80f',
+      strokeOpacity: 0.5,
+      strokeWeight: 1,
+      fillColor: '#edc80f',
       fillOpacity: 0.2
     });
 
